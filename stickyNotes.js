1c3844e180a5a1c5853414427f77ab125b967212
Ian <ian (at) ianrobinson.net>
2018-03-09 14:23:30 -0500

    Using array data type to save localStorage stickie data - object oriented way commented out below

--- stickyNotes.js
+++ stickyNotes.js
@@ -4,18 +4,34 @@ function init() {
   var button = document.getElementById("submitButton"); // Set variable to button
   button.onclick = getSticky; // Assign click event of button to getSticky function
   
-  for (var i = 0; i < localStorage.length; i += 1) {
-    var key = localStorage.key(i);
-    // We check to ensure substring starts with "sticky" so we don't display
-    //  localStorage information that the user didn't store as a sticky note.
-    if (key.substring(0, 6) == "sticky") {
-      var value = localStorage.getItem(key);
-      
-      // We know our keys are user generated and want to display them on the site for user.
-      addStickyNoteToDOM(value);
-    }
+  // Here we use Array to write data to the localStorage
+  var stickies = localStorage["stickies"]; // First we set a variable to be equal to the stickies array object in localStorage
+  if (!stickies) { // Here we check if it exists, if it doesn't we set it equal to an empty array
+    stickies = [];
+    localStorage.setItem('stickies', JSON.stringify(stickies));
+  } else {
+    stickies = JSON.parse(stickies);
   }
   
+  for (var i = 0; i < stickies.length; i += 1) { // Now we iterate through the stickies array
+    var key = stickies[i];
+    var value = localStorage[key];
+    addStickyToDOM(value);
+  }
+  
+  // // We initially used this JSON function to add to the localStorage object
+  // for (var i = 0; i < localStorage.length; i += 1) {
+  //   var key = localStorage.key(i);
+  //   // We check to ensure substring starts with "sticky" so we don't display
+  //   //  localStorage information that the user didn't store as a sticky note.
+  //   if (key.substring(0, 6) == "sticky") {
+  //     var value = localStorage.getItem(key);
+  // 
+  //     // We know our keys are user generated and want to display them on the site for user.
+  //     addStickyNoteToDOM(value);
+  //   }
+  // }
+  // 
   // More init() functionality should go here
 }
 // // Just me practicing writing this function so I can do it by memory
@@ -46,6 +62,7 @@ function addStickyNoteToDOM(userInput) {
   listItemStyle.innerHTML = userInput; // Adds the values from the user's localStorage array that starts with 'sticky' to the inner HTML of an element
   listItem.appendChild(listItemStyle); // Adds the contents of the <span>/listItemStyle to the <li>/listItem
   noteList.appendChild(listItem); // Adds the contents of the <li>/listItem to the <ul>/noteList
+  console.log(userInput, "<-userInput");
 }
 
 function getSticky() {
